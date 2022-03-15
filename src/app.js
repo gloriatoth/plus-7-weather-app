45c23a669cb3eb91c1570091ef59bce33271a808
Gloria Toth <gloria.toth@emilfrey.ch>
2022-03-15 16:02:47 +0100

    Styling fixed

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
+  console.log(response.data);
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
   let descriptionElement = document.querySelector("#description");
