bf7805bb90676eee3522755e956a2805508e0e0b
Gloria Toth <gloria.toth@emilfrey.ch>
2022-07-18 10:45:29 +0200

    API updated

--- src/app.js
+++ src/app.js
@@ -65,7 +65,7 @@ function displayForecast(response) {
 }
 
 function getForcast(coordinates) {
-  let apiKey = "5c67d9771dba3f2ffddccbc1dceb6a67";
+  let apiKey = "ee3665ed0666854a92c1030dbbb008b8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayForecast);
